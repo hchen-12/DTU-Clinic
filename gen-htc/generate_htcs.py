36a409fc68ec5ffb825fbef759643c08caa04739
clay <clayson.briggs@gmail.com>
2023-12-12 13:38:11 -0500

    updated htc script, also added script to run htcs from Jenni

--- gen-htc/generate_htcs.py
+++ gen-htc/generate_htcs.py
@@ -18,7 +18,7 @@ rotorR = 0.75               # rotor radius
 for i in range(5):     
     w = [x*windSpeed/rotorR for x in range(5)]                  # list comprehension sets up the desired rotational velocities
     htc.new_htc_structure.constraint.bearing3.omegas = w[i]     # change rotational velocity in htc file
-    htc.set_name('test_'+str(i))                                # set name (I think this is more so for results files and things)
+    htc.set_name('test_'+str(i))                                # set name for log and results files
     outName = startPath+'/htc/htc_test_'+str(int(i))+'.htc'     # create name to save as
     print(outName)
     htc.save(outName)                                           # save htc files
