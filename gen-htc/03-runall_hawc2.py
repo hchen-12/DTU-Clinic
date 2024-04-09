b90d044e4c2c42803bef5591b7f27d1b17f6047b
clay <clayson.briggs@gmail.com>
2024-04-08 19:57:39 -0700

    added newest 1strut model, ran results

--- gen-htc/03-runall_hawc2.py
+++ gen-htc/03-runall_hawc2.py
@@ -6,24 +6,27 @@ from pathlib import Path
 import subprocess
 
 
-MODEL_DIR = Path('../hawc2-main/VAWT_model/')  # model directory
+MODEL_DIR = Path('./') #../hawc2-main/3blade_1strut_model/')  # model directory
 HTC_RELDIR = Path('htc')                       # folder with htc files
-HAWC2_PATH = '../hawc2-main/HAWC2MB.exe'       # relative path to HAWC2 executable
+HAWC2_PATH = 'hawc2' #'../hawc2-main/HAWC2MB.exe'       # relative path to HAWC2 executable
 
 # get list of htc filepaths (defined with respect to currect directory)
-htc_files_list = [f for f in (MODEL_DIR / HTC_RELDIR).glob('*.htc') if 'test' in f.as_posix()] 
+htc_files_list = [f for f in (MODEL_DIR / HTC_RELDIR).glob('*.htc') if '20240408' in f.as_posix()] 
 
-for i, (h2_exe, htc_files) in enumerate(zip(HAWC2_PATH, htc_files_list)):
+for i in range(len(htc_files_list)):
+
+    h2_exe = HAWC2_PATH
+    htc_file = htc_files_list[i]
 
     print(f'\nExecuting run with {h2_exe}.\n')
 
     # iterate over files and run hawc2
-    for i, htc_file in enumerate(htc_files):
-        rel_path = HTC_RELDIR / htc_file.name  # path defined w.r.t. model directory
-        print(f' Running file {i+1} of {len(htc_files)}...')
-        proc = subprocess.run([h2_exe, rel_path], capture_output=True, cwd=MODEL_DIR)
-        if proc.returncode:
-            print('  FAILED! Error message:')
-            print(proc.stderr.decode())
-        else:
-            print('  Success.')
+    
+    rel_path = HTC_RELDIR / htc_file.name  # path defined w.r.t. model directory
+    print(f' Running file {i+1} of {len(htc_files_list)}...')
+    proc = subprocess.run([h2_exe, rel_path], capture_output=True, cwd=MODEL_DIR)
+    if proc.returncode:
+        print('  FAILED! Error message:')
+        print(proc.stderr.decode())
+    else:
+        print('  Success.')
--- gen-htc/generate_htcs.py
+++ gen-htc/generate_htcs.py
@@ -4,13 +4,13 @@ import numpy as np
 from lacbox.htc import HTCFile
 
 # initialize htc file
-startPath = '../hawc2-main/VAWT_model'                  # initial (relative) path to the git repo (make sure this is in your home folder) and to the model
+startPath = '../hawc2-main/3blade_1strut_model'                  # initial (relative) path to the git repo (make sure this is in your home folder) and to the model
 modelpath = Path(startPath)                             # path to the model
-fname =  modelpath / 'htc/H-rotor_3blades.htc'          # path from the model to the htc file
+fname =  modelpath / 'htc/H-rotor_3blades_hmc1strut.htc'          # path from the model to the htc file
 htc = HTCFile(fname, modelpath=modelpath)               # create htc file object
 
 # set constant parameters 
-windSpeed = 4               # wind speed
+windSpeed = 3               # wind speed
 htc.wind.wsp = windSpeed    # set wind speed in htc file
 rotorR = 0.75               # rotor radius
 
@@ -18,7 +18,7 @@ rotorR = 0.75               # rotor radius
 for i in range(5):     
     w = [x*windSpeed/rotorR for x in range(5)]                  # list comprehension sets up the desired rotational velocities
     htc.new_htc_structure.constraint.bearing3.omegas = w[i]     # change rotational velocity in htc file
-    htc.set_name('test_'+str(i))                                # set name for log and results files
-    outName = startPath+'/htc/htc_test_'+str(int(i))+'.htc'     # create name to save as
+    htc.set_name('wsp3_htc'+str(i))                                # set name for log and results files
+    outName = startPath+'/htc/wsp3_htc'+str(int(i))+'_20240408.htc'     # create name to save as
     print(outName)
     htc.save(outName)                                           # save htc files
